40ceacefc62e9389c65a8c4f00918b80c5938359
Iris <iristidjio@gmail.com>
2025-06-24 21:05:36 +0300

    Navbar Implementation

---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <Link href={"#home"} className="flex items-center gap-2">
+          <Image src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </Link>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={`#${link.id}`}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
